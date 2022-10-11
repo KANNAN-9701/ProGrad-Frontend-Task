30437aac37720f34e465bb390f872813565126be
KANNAN <kannanmech0907@gmail.com>
2022-10-11 16:52:36 +0530

    Added 2 files

---
+++ ChanceContent/JS.js
@@ -0,0 +1,8 @@
+function es6(){
+    document.getElementById("h").innerHTML="ES6";
+    document.getElementById("p").innerHTML="ES6 is the sixth edition of the ECMAScript (a trademarked scripting language specification defined by ECMA International). It was introduced in 2015.It introduced a new primitive data type 'symbol' for supporting unique values.";
+}
+function es5(){
+    document.getElementById("h").innerHTML="ES5";
+    document.getElementById("p").innerHTML="ES5 is the fifth edition of the ECMAScript (a trademarked scripting language specification defined by ECMA International).It was introduced in 2009.ES5 supports primitive data types that are string, number, boolean, null, and undefined.";
+}
---
+++ ChangeAttribute/JS.js
@@ -0,0 +1,12 @@
+function one(){
+    document.getElementById("bgi").style.backgroundImage="url('https://media.istockphoto.com/photos/city-at-night-where-light-shines-from-one-window-picture-id1362246742?b=1&k=20&m=1362246742&s=170667a&w=0&h=RjTmIDM5_tgHsLc346Lh48rn5pznChmVd0qzVlyeJL4=')";
+}
+function two(){
+    document.getElementById("bgi").style.backgroundImage="url('https://images.unsplash.com/photo-1494376877685-d3d2559d4f82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60')";
+}
+function three(){
+    document.getElementById("bgi").style.backgroundImage="url('https://images.unsplash.com/photo-1449156733864-dd5471bb7427?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=6')";
+}
+function show(){
+    document.getElementsByTagName("form")[0].style.visibility="visible";
+}
